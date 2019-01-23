f8c9ecd9db07f5f53e1398bab5244ba33076207e
Jonathan Neidel <github@jneidel.com>
2019-01-23 17:58:52 +0100

    Optimize performance
    From 50s to 15s for my 27 channels

--- lib/get-playlist-items.sh
+++ lib/get-playlist-items.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
-
 if [[ $1 = "--help" ]] || [[ $1 = "help" ]]; then
   echo "$ get-playlist-items.sh <playlist>"
   echo "Download playlist items from yt api,\ncompare the publishing date against 'LAST_UPDATE'\nand add those videos to 'videos'."
   echo ""
   echo "Parameters:"
   echo "$1: playlist to download"
+  echo "$2: youtube api key"
+  echo "$3: length of each playlist"
   echo ""
   echo "Example:"
   echo "$ get-playlist-items.sh UU0BAd8tPlDqFvDYBemHcQPQ"
@@ -14,35 +15,38 @@ if [[ $1 = "--help" ]] || [[ $1 = "help" ]]; then
 fi
 
 PLAYLIST=$1
+API_KEY=$2
+PLAYLIST_LEN=$3
 DIR=~/scripts/yt
 LIB="$DIR/lib"
-API_KEY=$(cat $DIR/env.json | jq .YOUTUBE_API_KEY | $LIB/trim-quotes.js)
-PLAYLIST_LEN=$(cat $DIR/env.json | jq .PLAYLIST_LEN)
+LAST_DATE=$(cat $DIR/LAST_UPDATE)
 
 if [[ $PLAYLIST = "UU0BAd8tPlDqFvDYBemHcQPQ" ]]; then
   PLAYLIST_LEN=15 # playlist order is messed up, with 5 new vids arent in range
 fi
 
 DATA=$(curl -s "https://www.googleapis.com/youtube/v3/playlistItems?key=$API_KEY&part=snippet&playlistId=$PLAYLIST&maxResults=$PLAYLIST_LEN")
-DATA=$(echo $DATA | $LIB/parse-json.js) # does work if directly chained
+DATA=$(echo $DATA | $LIB/compare-dates.js $LAST_DATE) # does work if directly chained
 
-LAST_DATE=$(cat $DIR/LAST_UPDATE)
-CHANNEL=$(echo $DATA | jq .items[0].snippet.channelTitle | $LIB/trim-quotes.js)
-echo "Trying: $CHANNEL"
+TEST=$(echo $DATA | jq .items[].length) # .length does not exist, but returns null for every item, seperated by \n, ideal for 'wc -l' :)
 
-PLAYLIST_LEN=$(echo $PLAYLIST_LEN-1 | bc) # index starts a 0
-for x in $(seq 0 $PLAYLIST_LEN)
-  do {
-    CURRENT_DATE=$(echo $DATA | jq .items[$x].snippet.publishedAt | $LIB/trim-quotes.js)
-    IS_AFTER=$(node $LIB/compare-dates.js $LAST_DATE $CURRENT_DATE )
+if [[ -n $TEST ]]; then
+  LENGTH=$(echo $DATA | jq .items[].length | wc -l) # reusing $TEST does not work (messes up format)
+  SEQ_LENGTH=$(echo $LENGTH-1 | bc)
+  CHANNEL=$(echo $DATA | jq .items[0].snippet.channelTitle | sed --expression='s/"//g')
 
-    if [[ $IS_AFTER == "true" ]]; then
-      TITLE=$(echo $DATA | jq .items[$x].snippet.title | $LIB/trim-quotes.js)
-      VIDEO_ID=$(echo $DATA | jq .items[$x].snippet.resourceId.videoId | $LIB/trim-quotes.js)
+  for x in $(seq 0 $SEQ_LENGTH)
+    do {
+      TITLE=$(echo $DATA | jq .items[$x].snippet.title | sed --expression='s/"//g')
+      VIDEO_ID=$(echo $DATA | jq .items[$x].snippet.resourceId.videoId | sed --expression='s/"//g')
       URL="https://www.youtube.com/watch?v=$VIDEO_ID"
       echo "$CHANNEL: $TITLE - $URL" >> $DIR/videos
-      echo "$CHANNEL: $TITLE - $URL"
-    fi
-  }
-done
+      #echo "$CHANNEL: $TITLE - $URL"
+    }
+  done
+
+  COUNTER=$(cat $DIR/COUNTER)
+  echo $COUNTER+$LENGTH | bc > $DIR/COUNTER
+fi
+
 
